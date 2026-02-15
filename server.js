fb788bc2c8e42caf47ff6e1ccd7e54735c9ffc5c
Chiquitapun <kwdn6505@gmail.com>
2026-02-15 21:01:07 +0100

    bro

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('trust proxy', 1);
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://leafy.github.io', 
+    origin: 'https://chiquitapun.github.io', 
     methods: ['POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
 }));
